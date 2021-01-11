10fbc8ad31677b113a247c2fa0dabe833747586b
wangfei <2596345940@qq.com>
2021-01-11 20:35:55 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+// 移动端1像素边框的问题？因为有的手机分辨率比较高，但是如果在页面上写border-bottom：1px；
+//在1倍屏中是1px， 但是在2倍屏中是2px，所以需要解决这个问题；
+import './assets/styles/border.css'
+
+//移动端有300毫秒点击延迟的问题：在移动端的某些机型上，某些浏览器上，当使用click事件后，会延迟
+//3000毫秒再执行，需要引入一个fastclick
+import fastClcik from 'fastclick';
 
 Vue.config.productionTip = false
+fastClcik.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +21,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+})//路由就是根据网址的不同，返回不同的内容给用户
---
+++ travel/src/pages/home/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>hello world</div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+
+<style scoped></style>
