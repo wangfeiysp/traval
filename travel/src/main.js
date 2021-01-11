c5fb4549120126ec496d59bc6e0d3033d52726bd
wangfei <2596345940@qq.com>
2021-01-11 21:56:18 +0800

    add header

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 移动端1像素边框的问题？因为有的手机分辨率比较高，但是如果在页面上写border-bottom：1px；
 //在1倍屏中是1px， 但是在2倍屏中是2px，所以需要解决这个问题；
 import './assets/styles/border.css'
@@ -11,6 +11,7 @@ import './assets/styles/border.css'
 //移动端有300毫秒点击延迟的问题：在移动端的某些机型上，某些浏览器上，当使用click事件后，会延迟
 //3000毫秒再执行，需要引入一个fastclick
 import fastClcik from 'fastclick';
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClcik.attach(document.body)
---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+};
+// 这里样式中，1 rem = html font-size = 50px;
+//0.02 rem = 1px;
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display:flex
+  line-height:.86rem
+  height: 0.86rem
+  background: $bgColor
+  color: #fff
+
+  .header-left
+    width: 0.64rem
+    float:left
+    .back-icon
+      text-align :center
+      font-size:.4rem
+
+  .header-input
+    flex:1
+    background :#fff
+    border-radius :0.1rem
+    margin-top :.12rem
+    padding-left : .2rem
+    height : .64rem
+    margin-left :.2rem
+    color:#ccc
+    line-height :.64rem
+
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align :center
+    .arrow-icon
+      font-size: .24rem
+      margin-left:-0.04rem
+</style>
