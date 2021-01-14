e99177d0da95fca82fae8e5473ee16855133e094
wangfei <2596345940@qq.com>
2021-01-14 22:04:41 +0800

    add detail

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/home.vue'
 import City from '@/pages/city/City.vue'
+import Detail from '@/pages/detail/Detail.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,15 @@ export default new Router({
       path:'/city',
       name: 'City',
       component: City
+    },
+    {
+      path:'/detail/:id',//这里是动态路由的写法，前面必须满足/detail/
+      name: 'Detail',
+      component: Detail
     }
-  ]
+  ],
+   // 这个意思是切换路由进入新页面的时候，每次都是从页面顶部开始；
+   scrollBehavior(to, from, savedPosition) {
+    return {x:0, y:0}
+  }
 })
